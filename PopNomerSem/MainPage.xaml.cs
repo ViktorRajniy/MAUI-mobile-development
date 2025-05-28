d6a8440c9d135e279bba0a3f5c8b014099559829
Виктор Оксингерт <rajniyviktor@yandex.ru>
2025-05-28 19:10:24 +0700

    Добавьте файлы проекта.

---
+++ PopNomerSem/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.ObjectModel;
+
+namespace PopNomerSem
+{
+    public partial class MainPage : ContentPage
+    {
+        private ObservableCollection<Note> _notes = [
+            new Note {Title = "qwe"},
+            new Note {Title = "wer"},
+            new Note {Title = "ert"},
+            ];
+
+        public ObservableCollection<Note> ShownNotes = [];
+
+        public MainPage()
+        {
+            InitializeComponent();
+            foreach (var note in _notes)
+            {
+                ShownNotes.Add(note);
+            }
+            NotesList.ItemsSource = ShownNotes;
+        }
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {            
+            var search = e.NewTextValue?.ToLower() ?? string.Empty;
+
+            if(search.Equals(string.Empty))
+            {
+                ShownNotes.Clear();
+                foreach (var note in _notes)
+                {
+                    ShownNotes.Add(note);
+                }
+            }
+            else
+            {
+                var filteredNotes = _notes
+                    .Where(n => n.Title.ToLower().Contains(search))
+                    .ToList();
+
+                ShownNotes.Clear();
+                foreach (var note in filteredNotes)
+                {
+                    ShownNotes.Add(note);
+                }
+            }
+
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ PopNomerSem/Note.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopNomerSem
+{
+    public class Note
+    {
+        private string _title = string.Empty;
+        public string Title
+        {
+            get { return _title; }
+            set { _title = value; }
+        }
+
+        private string _text = string.Empty;
+        public string Text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+
+        private DateTime _date = DateTime.Now;
+        public DateTime Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+    }
+}
